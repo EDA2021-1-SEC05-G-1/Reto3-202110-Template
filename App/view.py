0ad7d5a2a7453a84e115d7d02a00e4baaac59eb6
Sebastianc29 <78043175+Sebastianc29@users.noreply.github.com>
2021-04-21 23:33:42 -0500

    Update view.py

--- App/view.py
+++ App/view.py
@@ -1,4 +1,4 @@
-﻿"""
+"""
  * Copyright 2020, Departamento de sistemas y Computación, Universidad
  * de Los Andes
  *
@@ -24,6 +24,10 @@ import config as cf
 import sys
 import controller
 from DISClib.ADT import list as lt
+from DISClib.ADT import orderedmap as om
+from DISClib.ADT import map as mp
+from DISClib.DataStructures import orderedmapstructure as mo
+from DISClib.DataStructures import mapentry as me
 assert cf
 
 
@@ -35,12 +39,17 @@ operación solicitada
 """
 
 def printMenu():
+    print("\n")
+    print("*******************************************")
     print("Bienvenido")
-    print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("1- Cargar el catálogo")
+    print("2- Cargar información en el catalogo")
+    print("3- Caracterizar las reproducciones")
+    print("0-Salir")
+    print("*******************************************")
 
 catalog = None
-
+contexto= "context_content_features-small.csv"
 """
 Menu principal
 """
@@ -48,11 +57,39 @@ while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
-        print("Cargando información de los archivos ....")
 
+        print("\nInicializando....")
+        # cont es el controlador que se usará de acá en adelante
+        cont = controller.init()
     elif int(inputs[0]) == 2:
-        pass
+        print("Cargando información de los archivos ....")
+        controller.loadData(cont, contexto)
 
+        print("Hay "+str(lt.size(cont["tracks"]))+" tracks cargados.")
+       
+    elif int(inputs[0]) ==3:
+        caracteristica=str(input("Ingrese una caracteristica: "))
+        minimo=input("Ingrese el valor minimo del contenido: ")
+        maximo=input("Ingrese el valor minimo del contenido: ")
+       # nueva=lt.newList() #Nueva lista para empezar a meter para no repetir artistas
+      #  validacion=om.values(cont[caracteristica],minimo,maximo)#Hacer un filtrado de la caracteristica desde lo que quiere el usuario
+       # for x in range(1+lt.size(validacion)):
+        #    parte=lt.getElement(validacion,x)
+         #   peque=me.getValue(cont['user_id'])
+          #  artista=mp.get(peque,'user_id')
+           # if lt.isPresent(nueva,artista)==0:
+            #    lt.addLast(artista)
+      #  print(lt.size(validacion))
+       # print(lt.size(nueva))
+       #Para este requerimiento la idea era que a partir del mapa de los artistas de las llaves,
+       #Contar cuantas veces tiene la caracteristica deseada en sus tracks y meterse a sus valores
+       #Mirando que cumpla cada track con la caracteristica para poder sacar ese track en otro ordered map
+       #Basicamente crear otro om para poder luego hacer .keys() y lt.size()
+        #Otra solución propuesta fue que a partir del catalogo, crear un nuevo pedazo para el requerimiento en el mismo
+        #Con el fin de que a partir de un catalogo base mirar que no se repitan losa artistas para ir guardando los datos,
+        #Sin embargo, no entendimos ni logramos correctamente como sacar los datos del catalogo
+        #Ahora bien, logramos llegar a tener un catalogo en base a las caracteristicas del contexto en el que se carga correctamente cada dato
+        
     else:
         sys.exit(0)
 sys.exit(0)
